69a88b7fc764ee4d77773a1e75b4b9cdd5763dfe
CharaD7 <jijakahn6@gmail.com>
2023-01-25 21:40:26 +0000

    Added data for names array

--- data.ts
+++ data.ts
@@ -1,9 +1,9 @@
 interface Name {
   id: number;
   name: string;
-  gender: string;
-  popularity: string;
-  length: string;
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
 }
 
 export enum Gender {
@@ -21,3 +21,111 @@ export enum Length {
   ALL = 'All',
   SHORT = 'Short'
 }
+
+export const names: Name[] = [
+  {
+    id: 1,
+    name: 'Joy',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 2,
+    name: 'Joy',
+    gender: Gender.GIRL,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT,
+  },
+  {
+    id: 3,
+    name: 'Brian',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 4,
+    name: 'Emmanuel',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.LONG,
+  },
+  {
+    id: 5,
+    name: 'Darlington',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.LONG,
+  },
+  {
+    id: 6,
+    name: 'Helen',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 7,
+    name: 'Rosemond',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.LONG,
+  },
+  {
+    id: 8,
+    name: 'Angelina',
+    gender: Gender.GIRL,
+    popularity: Popularity.TRENDY,
+    length: Length.LONG,
+  },
+  {
+    id: 9,
+    name: 'Moses',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT,
+  },
+  {
+    id: 10,
+    name: 'Esther',
+    gender: Gender.GIRL,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT,
+  },
+  {
+    id: 11,
+    name: 'Jude',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 12,
+    name: 'Ruby',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 13,
+    name: 'Modeccai',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.SHORT,
+  },
+  {
+    id: 14,
+    name: 'Hephziba',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.LONG,
+  },
+  {
+    id: 16,
+    name: 'Emilia',
+    gender: Gender.GIRL,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT,
+  },
+];
