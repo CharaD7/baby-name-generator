f60f6d6cbdb1c12af729b81c74dd686df4d45048
CharaD7 <jijakahn6@gmail.com>
2023-01-28 09:01:56 +0000

    Added styles to Option.vue

--- app.vue
+++ app.vue
@@ -88,46 +88,6 @@
     position: relative;
   }
 
-  .option-container {
-    margin-bottom: 2rem;
-  }
-
-  .option {
-    background: white;
-    outline: 0.15rem solid rgb(249, 87, 89);
-    border: none;
-    padding: 0.75rem;
-    width: 12rem;
-    font-size: 1rem;
-    color: rgb(27, 60, 138);
-    cursor: pointer;
-    font-weight: 200;
-  }
-
-  .option-left {
-    border-radius: 1rem 0 0 1rem;
-  }
-
-  .option-right {
-    border-radius: 0 1rem 1rem 0;
-  }
-
-  .option-active {
-    background-color: rgb(249, 87, 89);
-    color: white;
-  }
-
-  .primary {
-    background-color: rgb(249, 87, 89);
-    color: white;
-    border-radius: 6.5rem;
-    border: none;
-    padding: 0.75rem 4rem;
-    font-size: 1rem;
-    margin-top: 1rem;
-    cursor: pointer;
-  }
-
   .cards-container {
     display: flex;
     margin-top: 3rem;
@@ -154,5 +114,16 @@
     color: rgb(27, 60, 138);
     border-radius: 0.5rem;
     background-color: white;
+  }
+
+  .primary {
+    background-color: rgb(249, 87, 89);
+    color: white;
+    border-radius: 6.5rem;
+    border: none;
+    padding: 0.75rem 4rem;
+    font-size: 1rem;
+    margin-top: 1rem;
+    cursor: pointer;
   }
 </style>
